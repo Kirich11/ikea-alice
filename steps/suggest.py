15e2ff824ddd0dc3060e52341da8e8208cb9f6f0
Kirichenko Nickita <nik@kodix.ru>
2019-09-18 21:06:43 +0300

    russian results

--- steps/suggest.py
+++ steps/suggest.py
@@ -2,6 +2,13 @@ class Suggest:
     def __init__ (self) :
         self.dictionary = 'suggest'
         self.name = 'suggest'
+        self.trans = {
+            'size' : 'размер',
+            'position' : 'поза сна', 
+            'temperature' : 'температура сна',
+            'price' : 'бюджет',
+            'hardness': 'жесткость'
+        }
 
     def getName (self) :
         return self.name
@@ -16,7 +23,7 @@ class Suggest:
         result = "Ваши предпочтения:\r\n"
         d = userSession['answers']
         for k in d:
-            result +=  k + " " + str(d[k]) + "\r\n"
+            result +=  self.trans[k] + "––" + str(d[k]) + "\r\n"
         return result
     
     def getNextStep(self):
